4f1b2d45fd55ada0cbf946f6e3d36076b0f77695
fiyanoushin <fathimafiyanoushin@gmail.com>
2025-10-09 10:21:51 +0530

    Time: 41 ms (45.77%) | Memory: 12.6 MB (45.03%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        res=[]
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+        return res    
+      
+        
+        
